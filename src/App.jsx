8b6677cc7f1ce9868813c1c8f596af0f5cf35e5f
Farlon Morales <moralesaguiar@live.com>
2025-07-18 16:53:25 +0200

    components update

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<ListRestaurant />} />
+
+      </Routes>
+        
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+//
+    
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import react from "react"
+
+function Footer {
+    <h1> this is the Footer </h1>
+
+}
+
+export default Footer
